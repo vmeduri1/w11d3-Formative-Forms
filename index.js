09b63d62d3502a47c3793f9061de7fbc3caf25ff
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-03-24 14:37:26 -0700

    Phase 5 started

--- index.js
+++ index.js
@@ -19,17 +19,7 @@ const users = [
   }
 ];
 
-app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  console.log(users);
-  res.render('index', {users})
-});
-
-app.get("/create", csrfProtection, (req, res) => {
-  res.render('create', { errors: [], csrfToken: req.csrfToken() });
-})
-
-app.post("/create", csrfProtection, (req, res) => {
+function errors(req, res, next){
   const { firstName, lastName, email, password, confirmedPassword } = req.body;
   const errors = [];
   if(!firstName){
@@ -47,9 +37,45 @@ app.post("/create", csrfProtection, (req, res) => {
   if(password !== confirmedPassword) {
     errors.push("The provided values for the password and password confirmation fields did not match.");
   }
+  req.errors = errors;
+  next();
+}
+
+app.get("/", (req, res) => {
+  // res.send("Hello World!");
+  console.log(users);
+  res.render('index', {users})
+});
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('create', { errors: [], csrfToken: req.csrfToken() });
+})
+
+app.post("/create", csrfProtection, errors, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword, errors } = req.body;
 
   if (errors.length > 0) {
     // console.log(errors);
+    res.render('create',{ errors,
+      firstName,
+      lastName,
+      email,
+      password,
+      csrfToken: req.csrfToken() });
+      return;
+  }
+  users.push({ firstName, lastName, email, id: users.length + 2 })
+  res.redirect(302, '/');
+})
+
+app.get("/create-interesting", csrfProtection, (req, res) => {
+  res.render("create-interesting", { errors: [], csrfToken: req.csrfToken()});
+})
+
+app.post("/create-interesting", csrfProtection, errors, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword, errors } = req.body;
+
+  if (errors.length > 0) {
     res.render('create',{ errors,
       firstName,
       lastName,
