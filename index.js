ba10d8f5cf8790ce182e7a26e784acfc5242c89f
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-24 14:52:50 -0500

    Phase 2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
+const csrfProtection = csrf({cookie:true})
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
@@ -18,6 +21,10 @@ app.get("/", (req, res) => {
   // res.send("Hello World!");
   res.render('index', {users})
 });
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('create', { csrfToken: req.csrfToken() });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
